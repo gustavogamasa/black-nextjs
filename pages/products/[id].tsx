f90b8489a8bddab090b15a4629f0889f3bc9833c
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2023-12-11 21:21:16 -0300

    Await fix

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,34 +1,67 @@
-import { GetStaticProps, NextPage } from "next";
-import { ProductType, fetchProductByID } from "../../src/services/products";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import {
+  ProductType,
+  fetchProductByID,
+  fetchProducts,
+} from "../../src/services/products";
 import { ReactNode } from "react";
+import Head from "next/head";
+import Header from "../../src/components/Header";
+import { Container } from "reactstrap";
+import ProductDetails from "../../src/components/ProductDetails";
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const id = context.params?.id
+  const id = context.params?.id;
 
-  if(typeof id === 'string'){
-    const product = fetchProductByID(id)
+  if (typeof id === "string") {
+    const product = await fetchProductByID(id);
 
     return {
       props: {
-        product
-      }
-    }
+        product,
+      },
+    };
   }
 
   return {
     redirect: {
       destination: "/products",
-      permanent: false
-    }
-  }
-}
+      permanent: false,
+    },
+  };
+};
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  const products = await fetchProducts();
+  const paths = products.map((product) => {
+    return { params: { id: product.id.toString() } };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 const Product: NextPage = (props: {
-  children?: ReactNode
-  product?: ProductType
+  children?: ReactNode;
+  product?: ProductType;
 }) => {
-  return(
-    
-  )
-}
+  return (
+    <>
+      <Head>
+        <title>{props.product!.name}</title>
+        <meta name="description" content={props.product!.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+
+      <Container className="mt-5">
+        <ProductDetails product={props.product!} />
+      </Container>
+    </>
+  );
+};
+
+export default Product
