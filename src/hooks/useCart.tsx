e95bc8f2af9ee3cc10ec0e6d4d103ddf9b88382e
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2023-12-14 16:01:51 -0300

    Context added to the app

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,12 +1,46 @@
-import { createContext } from "react"
+
+import { ReactNode, useContext, useEffect, useState } from "react"
 import { ProductType } from "../services/products"
 
 
-type CartContextType = {
-  cart: ProductType[]
-  addProduct: (product: ProductType) => void
-  removeProduct: (productId: number) => void
+
+export const CartContextProvider = (props: {
+  children: ReactNode
+}) => {
+  const [cart, setCart] = useState<ProductType[]>([])
+
+  useEffect(() => {
+    const storedCart = localStorage.getItem('shopping-cart')
+
+    if (storedCart) {
+      setCart(JSON.parse(storedCart))
+    }
+  }, [])
+
+  const addProduct = (product: ProductType) => {
+    const updatedCart = [...cart, product]
+    localStorage.setItem('shopping-cart', JSON.stringify(updatedCart))
+    setCart(updatedCart)
+  }
+
+  const removeProduct = (productId: number) => {
+    const productIndex = cart.findIndex(product => product.id === productId)
+
+    if (productIndex !== -1) {
+      const updatedCart = [...cart]
+      updatedCart.splice(productIndex, 1)
+      localStorage.setItem('shopping-cart', JSON.stringify(updatedCart))
+      setCart(updatedCart)
+    }
+  }
+
+  return (
+    <CartContextProvider
+      value={{ cart, addProduct, removeProduct }}
+    >
+      {props.children}
+    </CartContextProvider>
+  );
 }
 
-const CartContext = createContext<CartContextType>({} as CartContextType)
-
+export const useCart = () => useContext(CartContext)
