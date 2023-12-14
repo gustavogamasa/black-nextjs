5ac32bc69fe4888fd6eea4b62e1cd81f5e1142fb
Gustavo <56096596+gustavogamasa@users.noreply.github.com>
2023-12-14 16:14:12 -0300

    CartTable

---
+++ src/components/CartTable.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Button, Col, Row, Table } from "reactstrap";
+import { useCart } from "../hooks/useCart";
+import { ProductType } from "../services/products";
+
+type CartEntry = {
+  product: ProductType
+  quantity: number
+}
+
+const CartTableRow = (props: {
+  entry: CartEntry
+}) => {
+  const { addProduct, removeProduct } = useCart()
+
+  return (
+    <tr>
+      <td>
+        <Row className="align-items-center">
+          <Col xs={4} md={2} lg={1}>
+            <Image
+              src={props.entry.product.imageUrl}
+              alt={props.entry.product.name}
+              height={500}
+              width={600}
+            />
+          </Col>
+          <Col xs={8} md={10} lg={11}>
+            {props.entry.product.name}
+          </Col>
+        </Row>
+      </td>
+      <td>R$ {props.entry.product.price}</td>
+      <td>{props.entry.quantity}</td>
+      <td>R$ {(props.entry.product.price * props.entry.quantity)}</td>
+      <td>
+        <Button
+          color="primary"
+          size="sm"
+          onClick={() => addProduct(props.entry.product)}
+        >
+          +
+        </Button>
+        {' '}
+        <Button
+          color="danger"
+          size="sm"
+          onClick={() => removeProduct(props.entry.product.id)}
+        >
+          –
+        </Button>
+      </td>
+    </tr>
+  )
+}
